afc369597f55f17e39c3b1384a7d679551e2bca2
Debbie Michel and Luciano Oliveira <debbie@>
2017-08-15 13:47:21 -0700

    Add a list for fun-transportation and make it work

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,11 @@ $(document).ready(function(){
       var workTransportationMode = $(this).val();
       $("#work-responses").append(workTransportationMode + "<br>");
     });
+      $("#fun-responses").show();
+      $("input:checkbox[name=fun-transportation]:checked").each(function(){
+        var funTransportationMode = $(this).val();
+        $("#fun-responses").append(funTransportationMode + "<br>");
+    });
     $("#transportation_survey").hide();
 
   });
